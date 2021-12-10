b9978097e59e1862dd38c59a920e41aa582453a8
josgomgu <jgomez@mashumanoconsultores.com>
2021-12-09 19:56:18 -0500

    Proyecto Pizza Final - Back

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, redirect
+from persistencia import guardar_pedido
+
+app = Flask(__name__)
+
+@app.route("/pizza",methods=['POST'])
+def pizza():
+    nombre = request.form.get("nombre")
+    apellido = request.form.get("apellido")    
+    print("El solicitante del pedido es: "+nombre + " " + apellido)
+
+    with open("archivos/pedidos.txt", "w", encoding="utf-8") as file:
+        file.write("")
+        file.close()
+
+    guardar_pedido(nombre,apellido)    
+
+    return redirect("http://localhost/U1AF/solicita_pedido.html", code=302)
+
+    
+
---
+++ persistencia.py
@@ -0,0 +1,8 @@
+#Estudiante     : 	José Andrés Gómez
+#Módulo         :	1
+#Actividad Final :  Flask
+
+def guardar_pedido(nombres, apellidos):
+    with open("archivos/pedidos.txt", "a", encoding="utf-8") as file:
+        file.write("Pedido Almacenado para: " + nombres + " " + apellidos + "\n")
+        file.close()
