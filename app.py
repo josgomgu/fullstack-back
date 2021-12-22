3d15d6b92ec6a73801a362c8c2977fb1c582d660
josgomgu <jgomez@mashumanoconsultores.com>
2021-12-22 16:55:56 -0500

    Incorporación Metodo checksize

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """
     App.py Principal
 """
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, Response
 from persistencia import guardar_pedido
 
 app = Flask(__name__)
@@ -22,3 +22,16 @@ def pizza():
     guardar_pedido(nombre,apellido)
 
     return redirect("http://localhost/U1AF/solicita_pedido.html", code=302)
+
+@app.route("/checksize",methods=['POST'])
+def checksize():
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    """
+    size = request.form.get("tamano")
+    if(size=="S"):
+        mensaje = "No Disponible"
+    else:
+        mensaje = "Disponible" 
+
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
