5745e0af3699ebcbadde2d40683b8174571c6817
josgomgu <jgomez@mashumanoconsultores.com>
2021-12-22 16:26:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route("/pizza",methods=['POST'])
 def pizza():
+    """
+    Metodo Pizza
+    """
     nombre = request.form.get("nombre")
-    apellido = request.form.get("apellido")    
+    apellido = request.form.get("apellido")
     print("El solicitante del pedido es: "+nombre + " " + apellido)
 
     with open("archivos/pedidos.txt", "w", encoding="utf-8") as file:
         file.write("")
         file.close()
 
-    guardar_pedido(nombre,apellido)    
+    guardar_pedido(nombre,apellido)
 
     return redirect("http://localhost/U1AF/solicita_pedido.html", code=302)
-
-    
-
