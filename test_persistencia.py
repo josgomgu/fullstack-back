15454b5143dcdadee1675636a551890902b6997d
josgomgu <jgomez@mashumanoconsultores.com>
2021-12-22 16:39:47 -0500

    Ajuste de ruta de archivo

--- test_persistencia.py
+++ test_persistencia.py
@@ -6,7 +6,7 @@ def test_guardar_pedido():
     """
     Prueba general
     """
-    with open("pedidos.txt", "w+", encoding="utf-8") as file:
+    with open("archivos/pedidos.txt", "w+", encoding="utf-8") as file:
         persistencia.guardar_pedido("Pedro", "Gil de Diego")
         persistencia.guardar_pedido("Michael", "Jordan")
         firstline = file.readline()
